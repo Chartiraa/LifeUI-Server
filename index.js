42cdeab63d021b0ca1cca733ca440ba5892ecf0f
Chartira <akifcanzdmr@gmail.com>
2024-07-08 14:18:20 +0300

    Camera and SSH added

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import { Server } from "socket.io";
 import rclnodejs from 'rclnodejs';
+import express from "express";
+import http from "http";
 
-const io = new Server({
+import { Client } from "ssh2";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
   cors: {
     origin: "*"
   }
@@ -10,50 +17,98 @@ const io = new Server({
 var navbarData = { temperature: 0, humidity: 0, battery: 0 };
 var joystickData = { x: '0', y: '0' };
 
-NavbarTopics();
+// Global node reference
+let globalNode;
+
+async function initRclNode() {
+  await rclnodejs.init();
+  globalNode = rclnodejs.createNode('UI_Sensor_Subcriber');
+
+  // Subscriptions
+  globalNode.createSubscription('std_msgs/msg/String', 'temperature', (msg) => {
+    navbarData.temperature = msg.data;
+    //console.log(`Received message: ${typeof msg}`, msg);
+  });
+
+  globalNode.createSubscription('std_msgs/msg/String', 'humidity', (msg) => {
+    navbarData.humidity = msg.data;
+    //console.log(`Received message: ${typeof msg}`, msg);
+  });
+
+  globalNode.createSubscription('std_msgs/msg/String', 'battery', (msg) => {
+    navbarData.battery = msg.data;
+    //console.log(`Received message: ${typeof msg}`, msg);
+  });
+
+  rclnodejs.spin(globalNode);
+}
 
 io.on("connection", (socket) => {
   console.log("Baglanti kuruldu - server");
-
   console.log(socket.id);
 
   socket.on("Joystick", (data) => {
-
     joystickData = data;
 
-      const node = rclnodejs.createNode('publisher_example_node');
-      const publisher = node.createPublisher('std_msgs/msg/String', 'testmsgs');
-      publisher.publish({ data: JSON.stringify(joystickData) });
-      rclnodejs.spin(node);
+    // Publisher
+    const publisher = globalNode.createPublisher('std_msgs/msg/String', 'testmsgs');
+    publisher.publish({ data: JSON.stringify(joystickData) });
 
     console.log(data);
-  })
+  });
 
   socket.on("Stop", () => {
     console.log("Stop");
-  })
+  });
 
   socket.on("autonomousDrive", (data) => {
     console.log(data);
-  })
+  });
 
   socket.on("turnType", (data) => {
     console.log(data);
-  })
+  });
 
   socket.on("cameraSelect", (data) => {
     console.log(data);
-  })
+  });
 
   socket.on("speedFactor", (data) => {
     console.log(data);
-  })
+  });
 
-  socket.on("autonomousState", (data) => {
+  socket.on("status", (data) => {
     console.log(data);
-  })
-});
+  });
 
+  socket.on('executeCommand', (command) => {
+    const conn = new Client();
+    conn.on('ready', () => {
+      console.log('Client :: ready');
+      conn.exec(command, (err, stream) => {
+        if (err) {
+          socket.emit('commandOutput', `Error: ${err.message}`);
+          return;
+        }
+        let data = '';
+        stream.on('close', (code, signal) => {
+          console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+          conn.end();
+          socket.emit('commandOutput', data);
+        }).on('data', (chunk) => {
+          data += chunk;
+        }).stderr.on('data', (chunk) => {
+          data += chunk;
+        });
+      });
+    }).connect({
+      host: '10.116.60.146',
+      port: 22,
+      username: 'csa',
+      password: '236541'
+    });
+  });
+});
 
 setInterval(() => {
   io.emit("Navbar", navbarData);
@@ -61,25 +116,11 @@ setInterval(() => {
 
 io.listen(5000);
 
-function NavbarTopics() {
-  rclnodejs.init().then(() => {
-    const node = rclnodejs.createNode('UI_Sensor_Subcriber');
+server.listen(4000, () => {
+  console.log("Server is running on port 5000");
+});
 
-    node.createSubscription('std_msgs/msg/String', 'temperature', (msg) => {
-      navbarData.temperature = msg.data;
-      console.log(`Received message: ${typeof msg}`, msg);
-    });
-
-    node.createSubscription('std_msgs/msg/String', 'humidity', (msg) => {
-      navbarData.humidity = msg.data;
-      console.log(`Received message: ${typeof msg}`, msg);
-    });
-
-    node.createSubscription('std_msgs/msg/String', 'battery', (msg) => {
-      navbarData.battery = msg.data;
-      console.log(`Received message: ${typeof msg}`, msg);
-    });
-
-    rclnodejs.spin(node);
-  });
-}
+// Initialize ROS 2 node
+initRclNode().catch((err) => {
+  console.error('Failed to initialize ROS 2 node:', err);
+});
